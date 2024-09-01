ff6facc1ec19d31d887b0361edb429b4a91881c0
zeyd <1zeydinik@gmail/com>
2024-09-01 02:24:49 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import './App.css';
+import data from '../data';
+
+function App() {
+  const [selected, setSelected] = useState(null);
+
+  return <div className="app"></div>;
+}
+
+export default App;
