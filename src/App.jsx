b49e2cd5dd12fb3326993f1b7a05c3c593e6aa9f
zeyd <1zeydinik@gmail/com>
2024-09-01 06:09:55 +0200

    create single selection feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import { useState } from 'react';
-import './App.css';
 import data from '../data';
 
-function App() {
+export default function App() {
   const [selected, setSelected] = useState(null);
 
-  return <div className="app"></div>;
-}
+  const handleSingleSelection = (id) => {
+    setSelected(id === selected ? null : id);
+  };
 
-export default App;
+  return (
+    <div className="app">
+      {/* <button></button> */}
+      {data && data.length > 0 ? (
+        data.map((dataItem) => {
+          const { id, question, answer } = dataItem;
+          return (
+            <div key={id} className="menu-item">
+              <div
+                onClick={() => {
+                  handleSingleSelection(id);
+                }}
+                className="top"
+              >
+                <h3 className="question">{question}</h3>
+                <span className="icon">+</span>
+              </div>
+              {selected === id && <div className="answer">{answer}</div>}
+            </div>
+          );
+        })
+      ) : (
+        <div>no data found</div>
+      )}
+    </div>
+  );
+}
